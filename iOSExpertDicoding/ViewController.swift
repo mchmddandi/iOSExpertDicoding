d02fa861b90cdaa21385ce6081b32fd94b587a35
mchmddandi <mchmddandi@gmail.com>
2021-11-09 11:05:42 +0700

    Initial Commit

---
+++ iOSExpertDicoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSExpertDicoding
+//
+//  Created by Mochamad Dandi on 09/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
